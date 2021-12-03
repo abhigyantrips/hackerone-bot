2c9c701db3d376936287db549f171fa566030c50
Abhigyan Tripathi <abhigyantrips@gmail.com>
2021-12-03 22:21:35 +0530

    `reports-cmd` - Lint code with `black`.

--- slash-cmds/slash-cmd-reports.py
+++ slash-cmds/slash-cmd-reports.py
@@ -9,21 +9,21 @@ import dateutil.parser as dp
 auth = (os.getenv("HACKERONE_USERNAME"), os.getenv("HACKERONE_API_KEY"))
 headers = {"Accept": "application/json"}
 
-class SlashReports(commands.Cog):
 
+class SlashReports(commands.Cog):
     def __init__(self, client):
         self.client = client
-    
+
     @commands.slash_command()
     async def reports(self, ctx, id: str):
-        
+
         """Gets a report from HackerOne, as per specified ID.
 
         Parameters
         ----------
         id: The ID of the report to get.
         """
-        
+
         reportsjson = (
             requests.get(
                 f"https://api.hackerone.com/v1/reports/{id}",
@@ -66,14 +66,21 @@ class SlashReports(commands.Cog):
         except KeyError:
             pass
 
-        if "default" in reportsjson['data']['relationships']['reporter']['data']['attributes']['profile_picture']['62x62']:
+        if (
+            "default"
+            in reportsjson["data"]["relationships"]["reporter"]["data"]["attributes"][
+                "profile_picture"
+            ]["62x62"]
+        ):
             reporterpic = "https://hackerone.com/assets/avatars/default-71a302d706457f3d3a31eb30fa3e73e6cf0b1d677b8fa218eaeaffd67ae97918.png"
         else:
-            reporterpic = reportsjson['data']['relationships']['reporter']['data']['attributes']['profile_picture']['62x62']
+            reporterpic = reportsjson["data"]["relationships"]["reporter"]["data"][
+                "attributes"
+            ]["profile_picture"]["62x62"]
 
-        bounty=0
-        for amount in reportsjson['data']['relationships']['bounties']['data']:
-            bounty += float(amount['attributes']['awarded_amount'])
+        bounty = 0
+        for amount in reportsjson["data"]["relationships"]["bounties"]["data"]:
+            bounty += float(amount["attributes"]["awarded_amount"])
 
         embed = disnake.Embed(
             title=f"#{id} - {reportsjson['data']['attributes']['title']} ({(reportsjson['data']['attributes']['state']).capitalize()})",
@@ -89,33 +96,41 @@ class SlashReports(commands.Cog):
         )
 
         embed.add_field(
-            name="Created at", 
-            value=f"<t:{round(dp.parse(reportsjson['data']['attributes']['created_at']).timestamp())}:f>" or "-",
+            name="Created at",
+            value=f"<t:{round(dp.parse(reportsjson['data']['attributes']['created_at']).timestamp())}:f>"
+            or "-",
             inline=True,
         )
         embed.add_field(
             name="Disclosed at",
-            value=f"<t:{round(dp.parse(reportsjson['data']['attributes']['disclosed_at']).timestamp())}:f>" or "-",
+            value=f"<t:{round(dp.parse(reportsjson['data']['attributes']['disclosed_at']).timestamp())}:f>"
+            or "-",
             inline=True,
         )
         embed.add_field(
             name="Program",
-            value=reportsjson['data']['relationships']['program']['data']['attributes']['handle'] or "-",
+            value=f"[@{reportsjson['data']['relationships']['program']['data']['attributes']['handle']}](https://hackerone.com/{reportsjson['data']['relationships']['program']['data']['attributes']['handle']})",
             inline=False,
         )
         embed.add_field(
             name="Severity",
-            value=(reportsjson['data']['relationships']['severity']['data']['attributes']['rating'] or "-").capitalize(),
-            inline=True,
-        )
-        embed.add_field(
-            name="Score",
-            value=reportsjson['data']['relationships']['severity']['data']['attributes']['score'] or "-",
+            value=(
+                reportsjson["data"]["relationships"]["severity"]["data"]["attributes"][
+                    "rating"
+                ]
+                or "-"
+            ).capitalize(),
             inline=True,
         )
         embed.add_field(
             name="Bounty",
-            value=(reportsjson['data']['relationships']['bounties']['data'][0]['attributes']['awarded_currency'] or "") + f" {bounty}",
+            value=(
+                reportsjson["data"]["relationships"]["bounties"]["data"][0][
+                    "attributes"
+                ]["awarded_currency"]
+                or ""
+            )
+            + f" {bounty}",
             inline=True,
         )
 
@@ -125,6 +140,7 @@ class SlashReports(commands.Cog):
         )
 
         await ctx.response.send_message(embed=embed)
+
 
 def setup(client):
     client.add_cog(SlashReports(client))
