66c1ea1dfa88ebea71f85cdf6a01f7f78a283fe7
Abhigyan Tripathi <abhigyantrips@gmail.com>
2021-12-03 22:21:57 +0530

    `utils-cmd` - Add 'help' command.

--- basic-cmds/basic-cmd-utils.py
+++ basic-cmds/basic-cmd-utils.py
@@ -1,8 +1,10 @@
 import disnake
 from disnake.ext import commands
 
-class PrefixUtils(commands.Cog):
+import datetime
 
+
+class PrefixUtils(commands.Cog):
     def __init__(self, client):
         self.client = client
 
@@ -11,10 +13,49 @@ class PrefixUtils(commands.Cog):
         await ctx.send(
             f"<:h1hourglass:915993528813912074>  **Bot latency is **`{round(self.client.latency*1000)}ms`"
         )
-    
+
     @commands.command()
     async def help(self, ctx):
-        await ctx.send("The bot is currently under development.")
+
+        """Returns the help message for the bot."""
+
+        embed = disnake.Embed(
+            title="HackerOne Bot [!h1]",
+            description="An unofficial bot for interacting with the HackerOne API.",
+            color=0x303136,
+            url="https://abhigyantrips.dev/hackerone-bot",
+            timestamp=datetime.datetime.now(),
+        )
+
+        embed.set_author(
+            name="Abhigyan Trips",
+            url="https://github.com/abhigyantrips/hackerone-bot",
+            icon_url="https://i.imgur.com/Q0Zj5V4.jpg",
+        )
+
+        embed.set_thumbnail(
+            url="https://i.imgur.com/NSuNpoX.png",
+        )
+
+        embed.add_field(
+            name="",
+            value="```\n!h1 help (/help)\n└── Returns this embed.\n\n!h1 ping (/ping)\n└── Returns the latency of the HackerOne bot.\n\n!h1 user <username> (/user)\n└── Gets information about a HackerOne user.\n\n!h1 reports <id> (/reports)\n└── Gets a report from HackerOne, as per specified ID.```",
+            inline=False,
+        )
+
+        embed.add_field(
+            name="Contribution and Issues",
+            value="You can suggest features, report issues and contribute to the bot's source code via the [GitHub repository](https://github.com/abhigyantrips/hackerone-bot).",
+            inline=False,
+        )
+
+        embed.set_footer(
+            text=f"Requested by {ctx.author.name}",
+            icon_url=ctx.author.avatar.url,
+        )
+
+        await ctx.send(embed=embed)
+
 
 def setup(client):
     client.add_cog(PrefixUtils(client))
--- slash-cmds/slash-cmd-utils.py
+++ slash-cmds/slash-cmd-utils.py
@@ -1,32 +1,64 @@
 import disnake
 from disnake.ext import commands
 
-class SlashUtils(commands.Cog):
+import datetime
 
+
+class SlashUtils(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.slash_command(
-        name="h1"
-    )
-    async def h1(self, ctx):
-        pass
-
-    @h1.sub_command(
-        name="ping",
-        description="Returns the latency of the bot.",
-    )
+    @commands.slash_command()
     async def ping(self, ctx):
+
+        """Returns the latency of the HackerOne Bot"""
+
         await ctx.response.send_message(
             f"<:h1hourglass:915993528813912074>  **Bot latency is **`{round(self.client.latency*1000)}ms`"
         )
-    
-    @h1.sub_command(
-        name="help",
-        description="Returns the help message for the bot.",
-    )
+
+    @commands.slash_command()
     async def help(self, ctx):
-        await ctx.response.send_message("The bot is currently under development.")
+
+        """Returns the help message for the bot."""
+
+        embed = disnake.Embed(
+            title="HackerOne Bot [!h1]",
+            description="An unofficial bot for interacting with the HackerOne API.",
+            color=0x303136,
+            url="https://abhigyantrips.dev/hackerone-bot",
+            timestamp=datetime.datetime.now(),
+        )
+
+        embed.set_author(
+            name="Abhigyan Trips",
+            url="https://github.com/abhigyantrips/hackerone-bot",
+            icon_url="https://i.imgur.com/Q0Zj5V4.jpg",
+        )
+
+        embed.set_thumbnail(
+            url="https://i.imgur.com/NSuNpoX.png",
+        )
+
+        embed.add_field(
+            name="",
+            value="```\n!h1 help (/help)\n└── Returns this embed.\n\n!h1 ping (/ping)\n└── Returns the latency of the HackerOne bot.\n\n!h1 user <username> (/user)\n└── Gets information about a HackerOne user.\n\n!h1 reports <id> (/reports)\n└── Gets a report from HackerOne, as per specified ID.```",
+            inline=False,
+        )
+
+        embed.add_field(
+            name="Contribution and Issues",
+            value="You can suggest features, report issues and contribute to the bot's source code via the [GitHub repository](https://github.com/abhigyantrips/hackerone-bot).",
+            inline=False,
+        )
+
+        embed.set_footer(
+            text=f"Requested by {ctx.author.name}",
+            icon_url=ctx.author.avatar.url,
+        )
+
+        await ctx.response.send_message(embed=embed)
+
 
 def setup(client):
     client.add_cog(SlashUtils(client))
