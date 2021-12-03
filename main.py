c1e61458cf347b4e67e688a338f5b9af9272ba0b
Abhigyan Tripathi <abhigyantrips@gmail.com>
2021-12-03 09:45:03 +0530

    `main.py` - Add cog loading.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import disnake
 from disnake.ext import commands
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 import os
@@ -11,7 +12,27 @@ client = commands.Bot(
     help_command=None,
     test_guilds=[int(os.getenv("TEST_GUILD"))],
     strip_after_prefix=True,
+    reload=True,
 )
+
+print("---------- BASIC COMMANDS ----------")
+for filename in os.listdir("./basic-cmds"):
+    try:
+        if filename.endswith(".py"):
+            client.load_extension(f"basic-cmds.{filename[:-3]}")
+            print(f"|  Loaded {filename}.")
+    except Exception as e:
+        print(f"|  Could not load {filename} due to exception: \n{e}")
+
+print("---------- SLASH COMMANDS ----------")
+for filename in os.listdir("./slash-cmds"):
+    try:
+        if filename.endswith(".py"):
+            client.load_extension(f"slash-cmds.{filename[:-3]}")
+            print(f"|  Loaded {filename}.")
+    except Exception as e:
+        print(f"|  Could not load {filename} due to exception: \n{e}")
+
 
 @client.event
 async def on_ready():
@@ -23,9 +44,6 @@ async def on_ready():
     )
     print(f"The bot is online. \nLogged in as {client.user}({client.user.id}).")
 
-@client.command()
-async def ping(ctx):
-    await ctx.send(f"<:h1hourglass:915993528813912074>  **Bot latency is **`{round(client.latency*1000)}ms`")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 client.run(BOT_TOKEN)
