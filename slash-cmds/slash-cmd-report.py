aab4aa7a1130b409dab9743bb8cd0c6eb8ba5aac
Abhigyan Tripathi <abhigyantrips@gmail.com>
2021-12-04 15:10:59 +0530

    `report-cmd` - Rename commands.

--- slash-cmds/slash-cmd-report.py
+++ slash-cmds/slash-cmd-report.py
@@ -15,7 +15,7 @@ class SlashReports(commands.Cog):
         self.client = client
 
     @commands.slash_command()
-    async def reports(self, ctx, id: str):
+    async def report(self, ctx, id: str):
 
         """Gets information about a HackerOne report.
 
@@ -24,7 +24,7 @@ class SlashReports(commands.Cog):
         id: The ID of the report to get.
         """
 
-        reportsjson = (
+        reportjson = (
             requests.get(
                 f"https://api.hackerone.com/v1/reports/{id}",
                 auth=auth,
@@ -33,32 +33,32 @@ class SlashReports(commands.Cog):
         ).json()
 
         try:
-            if reportsjson["errors"][0]["status"] == 401:
+            if reportjson["errors"][0]["status"] == 401:
                 return await ctx.send(
                     f"<:h1cross:916006021909065728> **The bot is unauthorized to conduct this action.**",
                     ephemeral=True,
                 )
-            elif reportsjson["errors"][0]["status"] == 403:
+            elif reportjson["errors"][0]["status"] == 403:
                 return await ctx.send(
                     f"<:h1cross:916006021909065728> **The bot is forbidden to conduct this action.**",
                     ephemeral=True,
                 )
-            elif reportsjson["errors"][0]["status"] == 404:
+            elif reportjson["errors"][0]["status"] == 404:
                 return await ctx.send(
                     f"<:h1cross:916006021909065728> **Could not find HackerOne report: `#{id}`.**",
                     ephemeral=True,
                 )
-            elif reportsjson["errors"][0]["status"] == 429:
+            elif reportjson["errors"][0]["status"] == 429:
                 return await ctx.send(
                     f"<:h1cross:916006021909065728> **The bot is currently being ratelimited. Please try again later.**",
                     ephemeral=True,
                 )
-            elif reportsjson["errors"][0]["status"] == 500:
+            elif reportjson["errors"][0]["status"] == 500:
                 return await ctx.send(
                     f"<:h1cross:916006021909065728> **There was an internal server error. Please try again later.**",
                     ephemeral=True,
                 )
-            elif reportsjson["errors"][0]["status"] == 503:
+            elif reportjson["errors"][0]["status"] == 503:
                 return await ctx.send(
                     f"<:h1cross:916006021909065728> **The HackerOne API is currently offline. Please try again later.**",
                     ephemeral=True,
@@ -68,54 +68,54 @@ class SlashReports(commands.Cog):
 
         if (
             "default"
-            in reportsjson["data"]["relationships"]["reporter"]["data"]["attributes"][
+            in reportjson["data"]["relationships"]["reporter"]["data"]["attributes"][
                 "profile_picture"
             ]["62x62"]
         ):
             reporterpic = "https://hackerone.com/assets/avatars/default-71a302d706457f3d3a31eb30fa3e73e6cf0b1d677b8fa218eaeaffd67ae97918.png"
         else:
-            reporterpic = reportsjson["data"]["relationships"]["reporter"]["data"][
+            reporterpic = reportjson["data"]["relationships"]["reporter"]["data"][
                 "attributes"
             ]["profile_picture"]["62x62"]
 
         bounty = 0
-        for amount in reportsjson["data"]["relationships"]["bounties"]["data"]:
+        for amount in reportjson["data"]["relationships"]["bounties"]["data"]:
             bounty += float(amount["attributes"]["awarded_amount"])
 
         embed = disnake.Embed(
-            title=f"#{id} - {reportsjson['data']['attributes']['title']} ({(reportsjson['data']['attributes']['state']).capitalize()})",
+            title=f"#{id} - {reportjson['data']['attributes']['title']} ({(reportjson['data']['attributes']['state']).capitalize()})",
             color=0x303136,
             timestamp=datetime.datetime.now(),
             url=f"https://hackerone.com/reports/{id}",
         )
 
         embed.set_author(
-            name=f"{reportsjson['data']['relationships']['reporter']['data']['attributes']['name']} ({reportsjson['data']['relationships']['reporter']['data']['attributes']['username']})",
+            name=f"{reportjson['data']['relationships']['reporter']['data']['attributes']['name']} ({reportjson['data']['relationships']['reporter']['data']['attributes']['username']})",
             icon_url=reporterpic,
-            url=f"https://hackerone.com/{reportsjson['data']['relationships']['reporter']['data']['attributes']['username']}",
+            url=f"https://hackerone.com/{reportjson['data']['relationships']['reporter']['data']['attributes']['username']}",
         )
 
         embed.add_field(
             name="Created at",
-            value=f"<t:{round(dp.parse(reportsjson['data']['attributes']['created_at']).timestamp())}:f>"
+            value=f"<t:{round(dp.parse(reportjson['data']['attributes']['created_at']).timestamp())}:f>"
             or "-",
             inline=True,
         )
         embed.add_field(
             name="Disclosed at",
-            value=f"<t:{round(dp.parse(reportsjson['data']['attributes']['disclosed_at']).timestamp())}:f>"
+            value=f"<t:{round(dp.parse(reportjson['data']['attributes']['disclosed_at']).timestamp())}:f>"
             or "-",
             inline=True,
         )
         embed.add_field(
             name="Program",
-            value=f"[@{reportsjson['data']['relationships']['program']['data']['attributes']['handle']}](https://hackerone.com/{reportsjson['data']['relationships']['program']['data']['attributes']['handle']})",
+            value=f"[@{reportjson['data']['relationships']['program']['data']['attributes']['handle']}](https://hackerone.com/{reportjson['data']['relationships']['program']['data']['attributes']['handle']})",
             inline=False,
         )
         embed.add_field(
             name="Severity",
             value=(
-                reportsjson["data"]["relationships"]["severity"]["data"]["attributes"][
+                reportjson["data"]["relationships"]["severity"]["data"]["attributes"][
                     "rating"
                 ]
                 or "-"
@@ -125,7 +125,7 @@ class SlashReports(commands.Cog):
         embed.add_field(
             name="Bounty",
             value=(
-                reportsjson["data"]["relationships"]["bounties"]["data"][0][
+                reportjson["data"]["relationships"]["bounties"]["data"][0][
                     "attributes"
                 ]["awarded_currency"]
                 or ""
